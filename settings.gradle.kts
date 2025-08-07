48d2039b797a4b69285c0f82a362e5d70bb55f3a
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-08-07 17:04:17 +0200

    feat(release): Add Shadow plugin and Maven publishing configuration for Otis Velocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("micronaut", "4.5.4")
             version("annotations", "26.0.2")
+            version("shadow", "9.0.0")
             version("velocity", "3.4.0-SNAPSHOT")
 
             library(
@@ -28,6 +29,7 @@ dependencyResolutionManagement {
 
             plugin("micronaut.application", "io.micronaut.application").versionRef("micronaut")
             plugin("micronaut.aot", "io.micronaut.aot").versionRef("micronaut")
+            plugin("shadow", "com.gradleup.shadow").versionRef("shadow")
         }
     }
 }
--- velocity-plugin/build.gradle.kts
+++ velocity-plugin/build.gradle.kts
@@ -1,9 +1,68 @@
 plugins {
     id("java")
+    alias(libs.plugins.shadow)
+    `maven-publish`
 }
 
 dependencies {
     implementation(project(":client"))
     compileOnly(libs.velocity.api)
     annotationProcessor(libs.velocity.api)
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+
+        version = rootProject.version as String
+        artifactId = "otis-plugin"
+        groupId = rootProject.group as String
+        pom {
+            name = "Otis Velocity Plugin"
+            description = "The client plugin for Otis."
+            url = "https://github.com/OneLiteFeatherNET/Otis"
+            licenses {
+                license {
+                    name = "The Apache License, Version 2.0"
+                    url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                }
+            }
+            developers {
+                developer {
+                    id = "theevilreaper"
+                    name = "Steffen Wonning"
+                    email = "steffenwx@gmail.com"
+                }
+                developer {
+                    id = "themeinerlp"
+                    name = "Phillipp Glanz"
+                    email = "p.glanz@madfix.me"
+                }
+            }
+            scm {
+                connection = "scm:git:git://github.com:OneLiteFeatherNET/Otis.git"
+                developerConnection = "scm:git:ssh://git@github.com:OneLiteFeatherNET/Otis.git"
+                url = "https://github.com/OneLiteFeatherNET/Otis"
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    // Those credentials need to be set under "Settings -> Secrets -> Actions" in your repository
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                }
+            }
+
+            name = "OneLiteFeatherRepository"
+            val releasesRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-releases")
+            val snapshotsRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
+            url =
+                if (version.toString().contains("SNAPSHOT") || version.toString().contains("BETA") || version.toString()
+                        .contains("ALPHA")
+                ) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
 }
